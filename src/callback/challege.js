cd9516d1e806a856648bad6dfa806c605df19a2b
Charles <charlesgmont@Charless-MacBook-Pro.local>
2021-09-01 11:27:18 -0500

    Segunda parte y final del challenge callback

--- src/callback/challege.js
+++ src/callback/challege.js
@@ -1,5 +1,7 @@
 //Instancia de llamado a una API con JS Y XMMLH
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API= 'https://rickandmorty.com/api/character/';
+
 
 function fetchData(url_api, callback) {
     //peticion e instanciado htmlll request 
@@ -17,5 +19,19 @@ function fetchData(url_api, callback) {
         }
         
     }
+    
     xhttp.send();
 }
+
+fetchData(API, function (error1, data1) {
+    if (error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function(error2, data2) {
+        if(error2) return console.error(error2);
+        fetchData(daa2.origin.url, function (error3, data3) {
+            if (error3) return console.error(error3);
+            console.log(data.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        })
+    })
+})
