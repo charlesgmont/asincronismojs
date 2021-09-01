c7e0ab4176b8eb6b92d3cba4656b76e0936c3d56
Charles <charlesgmont@Charless-MacBook-Pro.local>
2021-09-01 10:37:10 -0500

    Clase 4 primer setTimeout

---
+++ src/callback/index.js
@@ -0,0 +1,27 @@
+//EJ de func q llama otra function
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2);
+}
+
+console.log(calc(2,2,sum));
+
+// Funtion Async con setTimeout y callback 
+
+function date(callback) {
+    console.log(new Date);
+    setTimeout(function () {
+        let date = new Date;
+        callback(date);
+    }, 3000)
+}
+
+function printDate (dateNow) {
+    console.log(dateNow);
+}
+
+date(printDate);
